9fe5184903fd59284ad62fb62e9d2b1a66ea538e
Familia <Familia@Familia-PC>
2020-10-10 23:30:56 -0500

    Clase usuario

--- ArmaloTuMismo/src/armalotumismo/Usuario.java
+++ ArmaloTuMismo/src/armalotumismo/Usuario.java
@@ -9,11 +9,17 @@ package armalotumismo;
  *
  * @author Familia
  */
-public class ArmaloTuMismo {
+public class Usuario {
 
     /**
      * @param args the command line arguments
      */
+    
+    private String Userid;
+    private String Password;
+    private boolean LoginStatus;
+    private String Birthday;
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
