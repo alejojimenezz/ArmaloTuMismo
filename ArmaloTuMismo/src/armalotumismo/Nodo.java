eae3955bb75424c80ec21700e6278ed562c62a2e
jgarn <jgarn@LAPTOP-PGT02MIA>
2020-10-16 20:00:02 -0500

    Clase nodo

---
+++ ArmaloTuMismo/src/armalotumismo/Nodo.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package armalotumismo;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Nodo {
+    //Algoritmo para implementar doblemente enlazada
+    Object value;
+    Nodo next;
+    public Nodo(Object value) {
+        this.value = value;
+        this.next=null;
+    
+    }
+    
+    public void link(Nodo n, Nodo p){
+    next=n;
+ 
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public Nodo getNext() {
+        return next;
+    }
+
+    public void setNext(Nodo next) {
+        this.next = next;
+    }
+    
+    
+    
+
+}
